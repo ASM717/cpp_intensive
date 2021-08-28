2eb73116194abbe8a614f6483e675a3e3b29f411
Stepan <stepan151@gmail.com>
2021-08-28 19:16:37 +0300

    ex00

---
+++ day02/ex00/Fixed.cpp
@@ -0,0 +1,11 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	m_point_value = 0;
+}
+
+Fixed::~Fixed(){
+	// std::cout << "Default destructor called" << std::endl;
+}
--- day02/ex00/Fixed.hpp
+++ day02/ex00/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 private:
@@ -10,6 +12,8 @@ public:
 	Fixed();
 
 	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
 };
 
 #endif
