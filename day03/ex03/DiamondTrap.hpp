cd5043571cefe69951e3ac2e0846d7f2dd0df419
Allyson Muriel <amuriel@mi-a1.msk.21-school.ru>
2021-09-01 17:51:39 +0300

    ex03

---
+++ day03/ex03/DiamondTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+class DiamondTrap : public FragTrap, public ScavTrap
+{
+private:
+
+public:
+	DiamondTrap();
+
+	~DiamondTrap();
+};
