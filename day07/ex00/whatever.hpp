5f454570a75bd09a785b7e2453320c59d90a1c93
Stepan <stepan151@gmail.com>
2021-10-10 14:56:29 +0300

    template ex00 ready

--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -3,4 +3,25 @@
 
 #include <iostream>
 
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b) ? a : b;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+T swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+	return (tmp);
+}
+
 #endif
