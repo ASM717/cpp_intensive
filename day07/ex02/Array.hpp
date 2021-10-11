d16183bee886170e497e0e6350ff5c4d2db928e3
Stepan <56968976+ASM717@users.noreply.github.com>
2021-10-11 18:04:56 +0300

    day07

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -3,8 +3,96 @@
 
 #include <iostream>
 
-// template <class T>
+template <typename T>
 
+class Array
+{
+private:
+	T *m_arr;
+	unsigned int m_n;
 
+public:
+	Array()
+	{
+		m_arr = NULL;
+		m_n = 0;
+	}
+	Array(unsigned int n)
+	{
+		m_arr = NULL;
+		m_n = 0;
+		try
+		{
+			if (n == 0)
+			{
+				throw ("Not Allocated Memory!");
+			}
+			m_arr = new T[n];
+			for (unsigned int i = 0; i < n; i++)
+			{
+				m_arr[i] = 0;
+			}
+			m_n = n;
+		}
+		catch(const char *e)
+		{
+			std::cerr << e << std::endl;
+		}
+	}
+	Array(Array const &ref)
+	{
+		m_arr = NULL;
+		m_n = 0;
+		*this = ref;
+	}
+	Array &operator=(Array const &ref)
+	{
+		if (this != &ref)
+		{
+			if (m_arr != NULL && m_n != 0)
+				delete[] m_arr;
+			if (ref.m_n)
+			{
+				m_arr = new T[ref.m_n];
+				for (unsigned int i = 0; i < ref.m_n; i++)
+				{
+					m_arr[i] = ref.m_arr[i];
+				}
+			}
+			else
+				m_arr = 0;
+			m_n = ref.m_n;
+		}
+		return *this;
+	}
+	~Array()
+	{
+		if (m_n)
+			delete [] m_arr;
+	}
+
+	T &operator [] (unsigned int index)
+	{
+		if (index >= m_n || index < 0)
+			throw SegFaultExecption();
+		return m_arr[index];
+	}
+
+	unsigned int size()
+	{
+		return m_n;
+	}
+
+	class SegFaultExecption : public std::exception
+	{
+	public:
+		SegFaultExecption() {}
+		virtual ~SegFaultExecption() throw() {}
+		virtual const char* what() const throw()
+		{
+			return "Seg Fault Exception!";
+		}
+	};
+};
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,6 +1,106 @@
+#include <iostream>
 #include "Array.hpp"
 
 int main()
 {
-	
+	Array<int> array(10);
+	std::cout << "integer array: ";
+	for (unsigned int i = 0; i < array.size(); i++)
+	{
+		array[i] = i;
+		std::cout << array[i] << " ";
+	}
+	unsigned int arr_size = array.size();
+	std::cout << "\t\tarray size = " << arr_size << std::endl;
+
+	Array<int> cpy_arr(array);
+	std::cout << "integer copy_array: ";
+	for (unsigned int i = 0; i < cpy_arr.size(); i++)
+	{
+		cpy_arr[i] = i;
+		std::cout << cpy_arr[i] << " ";
+	}
+	unsigned int arr_size_cpy = cpy_arr.size();
+	std::cout << "\tcpy_array size = " << arr_size_cpy << std::endl;
+
+	Array<char> arr_char(10);
+	std::cout << "char array: ";
+	for (unsigned int i = 0; i < arr_char.size(); i++)
+	{
+		arr_char[i] = 'a' + i;
+		std::cout << arr_char[i] << " ";
+	}
+	unsigned int char_arr_size = arr_char.size();
+	std::cout << "\t\tchar_array size = " << char_arr_size << std::endl;
+
+	Array<char> arr_char2;
+	arr_char2 = arr_char;
+
+	try
+	{
+		arr_char2[10];
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	//проверка на пустоту
+	Array<int> test(0);
+
+	system("leaks array");
+	return (0);
 }
+
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+// 	Array<int> numbers(MAX_VAL);
+// 	int* mirror = new int[MAX_VAL];
+// 	srand(time(NULL));
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		const int value = rand();
+// 		numbers[i] = value;
+// 		mirror[i] = value;
+// 	}
+// 	//SCOPE
+// 	{
+// 		Array<int> tmp = numbers;
+// 		Array<int> test(tmp);
+// 	}
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		if (mirror[i] != numbers[i])
+// 		{
+// 			std::cerr << "didn't save the same value!!" << std::endl;
+// 			return 1;
+// 		}
+// 	}
+// 	try
+// 	{
+// 		numbers[-2] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+// 	try
+// 	{
+// 		numbers[MAX_VAL] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		numbers[i] = rand();
+// 	}
+// 	delete [] mirror;
+// 	system ("leaks array");
+// 	return 0;
+// }
